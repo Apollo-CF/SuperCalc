db1a3d3a85590d8096dc499049b28716c075841d
Jonathon Chenvert <jchenvert1@gmail.com>
2020-05-13 14:43:05 -0700

    Initial project creation

---
+++ SuperCalc/SuperCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperCalc
+//
+//  Created by Jonathon Chenvert on 5/13/20.
+//  Copyright © 2020 Jonathon Chenvert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
