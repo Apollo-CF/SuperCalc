ae7559ebf5be94ac3a6ae6dcd1be611db6c18eb9
markFalcone <falconemark16@yahoo.com>
2020-05-28 00:55:39 -0700

    Fixed the outlets and added them to a colection

--- SuperCalc/SuperCalc/Views/CalculatorViewController.swift
+++ SuperCalc/SuperCalc/Views/CalculatorViewController.swift
@@ -10,32 +10,22 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
-    @IBOutlet weak var calculateLabel: UILabel!
+
+    @IBAction func OperationSelector(_ sender: UIButton) {
+        print (sender.currentTitle)
+    }
     
-    @IBOutlet weak var clearButton: UIButton!
-    @IBOutlet weak var positiveNegativeButton: UIButton!
-    @IBOutlet weak var percentageButton: UIButton!
-    @IBOutlet weak var divideButton: UIButton!
-    @IBOutlet weak var num7Button: UIButton!
-    @IBOutlet weak var num8Button: UIButton!
-    @IBOutlet weak var num9Button: UIButton!
-    @IBOutlet weak var multiplyButton: UIButton!
-    @IBOutlet weak var num4Button: UIButton!
-    @IBOutlet weak var num5Button: UIButton!
-    @IBOutlet weak var num6Button: UIButton!
-    @IBOutlet weak var subtractButton: UIButton!
-    @IBOutlet weak var num1Button: UIButton!
-    @IBOutlet weak var num2Button: UIButton!
-    @IBOutlet weak var num3Button: UIButton!
-    @IBOutlet weak var additionButton: UIButton!
-    @IBOutlet weak var num0Button: UIButton!
-    @IBOutlet weak var decimalButton: UIButton!
-    @IBOutlet weak var equalsButton: UIButton!
+    
+    @IBAction func NumberSelector(_ sender: UIButton) {
+        print (sender.currentTitle)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("hi")
         // Do any additional setup after loading the view.
     }
+
+    
 
 }
